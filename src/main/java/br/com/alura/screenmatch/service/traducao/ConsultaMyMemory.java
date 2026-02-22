8eb105fd39bdc2fbee503ff8cb108e5cfc42dbb8
Philipe <philipe.borges@gmail.com>
2026-02-22 20:33:03 -0300

    220226

---
+++ src/main/java/br/com/alura/screenmatch/service/traducao/ConsultaMyMemory.java
@@ -0,0 +1,29 @@
+package br.com.alura.screenmatch.service.traducao;
+
+
+import br.com.alura.screenmatch.service.ConsumoApi;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+public class ConsultaMyMemory {
+    public static String obterTraducao(String text) {
+        ObjectMapper mapper = new ObjectMapper();
+        ConsumoApi consumo = new ConsumoApi();
+
+
+        String texto = URLEncoder.encode(text, StandardCharsets.UTF_8);
+        String langpair = URLEncoder.encode("en|pt-br", StandardCharsets.UTF_8);
+        String url = "https://api.mymemory.translated.net/get?q=" + texto + "&langpair=" + langpair;
+
+        String json = consumo.obterDados(url);
+
+        try {
+
+            DadosTraducao traducao = mapper.readValue(json, DadosTraducao.class);
+            return traducao.responseData().translatedText();
+        } catch (Exception e) {
+            return text;
+        }
+    }
+}
